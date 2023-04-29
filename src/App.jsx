51c09929d71087fe6de547e92f76f5f38c46cece
Victor Hugo Souza Ferreira <ProjektVek@gmail.com>
2023-04-29 16:34:52 -0300

    Fixing Unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
-import styles from './App.module.css'
-import { Header } from './components/Header'
 import { AppContextProvider } from './contexts/AppContext'
-import { Home } from './pages/Home/Index'
 import { Router } from './Router'
-import { Tweet } from './Tweet.jsx'
 
 export default function App() {
 /*   return (
